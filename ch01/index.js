9745a64979bcf703b9ef713edbf0af541f45031e
Choiminseok18 <ft000130@gmail.com>
2024-04-12 15:00:22 +0900

    1장 타입스크립트 소개와 배경 학습

---
+++ ch01/index.js
@@ -0,0 +1,8 @@
+// index.js
+
+function sum1(a, b) {
+    return a + b;
+}
+
+var total1 = sum1(10, 20);
+total1.toFixed(2)
---
+++ ch01/index.ts
@@ -0,0 +1,7 @@
+// index.ts
+function sum2(a:number, b:number) {
+    return a + b;
+}
+
+var total2 = sum2(10, 20);
+total2.toFixed(2)
